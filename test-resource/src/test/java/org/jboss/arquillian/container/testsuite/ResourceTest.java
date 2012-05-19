ae631b9e92ddafae06f7b3fcb605d044af3315cb
Karel Piwko <kpiwko@redhat.com>
2012-05-19 14:03:55 +0200

    Fixed @Resource based test

--- test-resource/src/test/java/org/jboss/arquillian/container/testsuite/ResourceTest.java
+++ test-resource/src/test/java/org/jboss/arquillian/container/testsuite/ResourceTest.java
@@ -1,6 +1,11 @@
 package org.jboss.arquillian.container.testsuite;
 
+import java.io.File;
+
+import javax.annotation.Resource;
+
 import junit.framework.Assert;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -8,23 +13,20 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-import java.io.File;
-
 @RunWith(Arquillian.class)
 public class ResourceTest {
 
-    @Resource(mappedName = "java:jboss/datasources/ExampleDS")
-    DataSource ds;
+    @Resource(name = "resourceInjectionTestName")
+    String resource;
 
     @Deployment
     public static WebArchive createDeployment() {
-        return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../package-ejb-war/target/test.war"));
+        return ShrinkWrap.createFromZipFile(WebArchive.class, new File("../package-plain-war/target/test.war"));
     }
 
     @Test
     public void resourceTest() {
-        Assert.assertNotNull(ds);
+        Assert.assertNotNull("Resource from web.xml was injected", resource);
+        Assert.assertEquals("Resource value reflect web.xml content", "Hello World from an env-entry", resource);
     }
 }
